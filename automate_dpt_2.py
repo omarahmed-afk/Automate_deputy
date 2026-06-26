947db6953f46e1633894b778c11ad1a2b28e53b6
Omar Ahmed <omar.ahmed@cobsolution.com>
2026-06-26 08:07:04 -0700

    Run second automation for Yesterday

--- automate_dpt_2.py
+++ automate_dpt_2.py
@@ -59,8 +59,9 @@ FIRST_DATA_ROW = 2
 
 # ================= DATE FUNCTIONS =================
 def get_target_date():
-    tomorrow = datetime.now(NY).date() + timedelta(days=1)
-    return str(tomorrow)
+    yesterday = datetime.now(NY).date() - timedelta(days=1)
+    return str(yesterday)
+
 
 def get_day_range_unix(target_date):
     start_dt = datetime.strptime(target_date, "%Y-%m-%d").replace(tzinfo=NY)
